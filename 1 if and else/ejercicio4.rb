e205e814694af8d98e19702b160aaaed706b45f7
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-11-26 19:36:54 -0300

    add ejercicio 3 y 4

--- 1 if and else/ejercicio4.rb
+++ 1 if and else/ejercicio4.rb
@@ -1,6 +1,7 @@
 # Permitir que el usuario pueda ingresar la contraseña por teclado 
 # para que se cumpla la condición.
-
+print 'Ingresa tu contraseña: '
+password = gets.chomp
 if password == 'secreto'
   puts 'Acceso PERMITIDO! :)'
 else
