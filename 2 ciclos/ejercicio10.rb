79abf569ac314b77c283e39d0baf6262385d954b
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-12-02 12:47:14 -0300

    ejercicio 10 cycles 100%

--- 2 ciclos/ejercicio10.rb
+++ 2 ciclos/ejercicio10.rb
@@ -2,10 +2,13 @@
 # e imprimirlo reiteradamente hasta que el usuario ingrese la opcion numero 4.
 
 ready = 0
-while(ready == 0){
+while(ready == 0) do
   puts "Opción 1: blah"
   puts "Opción 2: blah"
   puts "Opción 3: blah"
   puts "Opción 4: Salir"
-  opcion = get.chomps.to_i
-}
+  opcion = gets.chomp.to_i
+  if opcion == 4
+  	break
+  end
+end
