bf816ddaa903a0336c31b4c6407d25bee0b948b0
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-12-02 14:30:24 -0300

    Ejercicio 3 listo anidado

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -1,4 +1,14 @@
-# Construir un programa que permita ingresar un número por teclado e imprimir
-# la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
+# Construir un programa que permita ingresar un numero por teclado e imprimir
+# la tabla de multiplicar del numero ingresado. Debe repetir la operacion hasta
 # que se ingrese un 0 (cero).
-# Ingrese un número (0 para salir): _
+# Ingrese un numero (0 para salir):
+puts 'Ingrese un numero distinto de 0 para continuar'
+number = gets.chomp.to_i
+while number != 0 do
+	puts 'Ingrese un numero para conocer su tabla de multiplicar (0 para salir)'
+	number = gets.chomp.to_i
+	for n in 1..12
+		result = number*n.to_i
+		puts " #{number} * #{n} = #{result}"
+	end	
+end
